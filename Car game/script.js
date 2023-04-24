1de7435a5abb8b1cb2b93035c508981d215c49be
TibiAHegyről <99481995+emoditibi@users.noreply.github.com>
2023-04-24 19:25:22 +0200

    Alap fájlok 2023.04.24

---
+++ Car game/script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    let jatekos = document.querySelector('#hero');
+
+    document.addEventListener('keydown', function(e) {
+      if (e.key == 'ArrowLeft') {
+        lepesBalra();
+      }
+      if (e.key == 'ArrowRight') {
+        lepesJobbra();
+      }
+    });
+  
+    function lepesBalra() {
+        jatekos = document.querySelector('#jatekos')
+        BalPos = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'))
+        BalPos -= 100;
+        if (BalPos >= 0) {
+            console.log(jatekos)
+            jatekos.style.left = BalPos + 'px';
+        }
+    }
+    function lepesJobbra() {
+        jatekos = document.querySelector('#jatekos')
+        JobbPos = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'))
+        JobbPos += 100;
+        if (JobbPos < 300) {
+
+            jatekos.style.left = JobbPos + 'px';
+        }
+    }
+  
+
+  
+  });
