7fa9492809cdee70739c45473a7109fb7bded30f
Adam <100288238+Juhaszadam00@users.noreply.github.com>
2023-04-25 13:05:42 +0200

    fejlesztve pt1
    fejlesztve pt1

--- Car game/script.js
+++ Car game/script.js
@@ -1,41 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    let jatekos = document.querySelector('#jatekos');
-    let animationDuration = 2000; 
-    let interval = setInterval(Gyorsitas, 6000); 
+  let jatekos = document.querySelector('#jatekos');
+  let animationDuration = 3000; 
+  let interval = setInterval(Gyorsitas, 6000); 
 
-    document.addEventListener('keydown', function(e) {
-      if (e.key == 'ArrowLeft') {
-        lepesBalra();
-      }
-      if (e.key == 'ArrowRight') {
-        lepesJobbra();
-      }
-    });
-  
-    function lepesBalra() {
-        jatekos = document.querySelector('#jatekos')
-        BalPos = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'))
-        BalPos -= 100;
-        if (BalPos >= 0) {
-            console.log(jatekos)
-            jatekos.style.left = BalPos + 'px';
-        }
+  document.addEventListener('keydown', function(e) {
+    if (e.key == 'ArrowLeft') {
+      lepesBalra();
     }
-    function lepesJobbra() {
-        jatekos = document.querySelector('#jatekos')
-        JobbPos = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'))
-        JobbPos += 100;
-        if (JobbPos < 300) {
-
-            jatekos.style.left = JobbPos + 'px';
-        }
+    if (e.key == 'ArrowRight') {
+      lepesJobbra();
     }
-  
+  });
 
-  function Gyorsitas() {
-    animationDuration -= 150; 
-    road.style.animationDuration = animationDuration + 'ms'; 
-    console.log(Gyorsitas)
+  function lepesBalra() {
+      jatekos = document.querySelector('#jatekos')
+      BalPos = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'))
+      BalPos -= 100;
+      if (BalPos >= 0) {
+          console.log(jatekos)
+          jatekos.style.left = BalPos + 'px';
+      }
+  }
+  function lepesJobbra() {
+      jatekos = document.querySelector('#jatekos')
+      JobbPos = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'))
+      JobbPos += 100;
+      if (JobbPos < 300) {
+
+          jatekos.style.left = JobbPos + 'px';
+      }
+  }
+
+
+function Gyorsitas() {
+  animationDuration -= 200; 
+  road.style.animationDuration = animationDuration + 'ms'; 
+  console.log(road.style.animationDuration)
 }
 });
