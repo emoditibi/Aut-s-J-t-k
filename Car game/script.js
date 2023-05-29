3e1762a7acea0bc3331a9edcf02414c3a2c84479
TibiAHegyről <99481995+emoditibi@users.noreply.github.com>
2023-05-29 15:51:48 +0200

    Update script.js

--- Car game/script.js
+++ Car game/script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   let jatekos = document.querySelector('#jatekos');
   let akadaly = document.querySelector('#akadaly');
-  let animationDuration = 4000; 
-  let interval = setInterval(Gyorsitas, 10000); 
-  pont = 0;
+  let pont = 0;
+  let akadalytop = 0;
+
   document.addEventListener('keydown', function(e) {
     if (e.key == 'ArrowLeft') {
       lepesBalra();
@@ -14,47 +14,37 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   function lepesBalra() {
-      jatekos = document.querySelector('#jatekos')
-      BalPos = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'))
-      BalPos -= 100;
-      if (BalPos >= 0) {
-          console.log(jatekos)
-          jatekos.style.left = BalPos + 'px';
-      }
+    let BalPos = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'));
+    BalPos -= 100;
+    if (BalPos >= 0) {
+      jatekos.style.left = BalPos + 'px';
+    }
   }
+
   function lepesJobbra() {
-      jatekos = document.querySelector('#jatekos')
-      JobbPos = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'))
-      JobbPos += 100;
-      if (JobbPos < 300) {
-
-          jatekos.style.left = JobbPos + 'px';
-      }
+    let JobbPos = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'));
+    JobbPos += 100;
+    if (JobbPos < 300) {
+      jatekos.style.left = JobbPos + 'px';
+    }
   }
+
   document.addEventListener('animationiteration', function() {
-  randomNumber = Math.floor(Math.random() * 3) * 100;
-  akadaly.style.left = randomNumber + 'px';
-})
-function Gyorsitas() {
-  animationDuration -= 150; 
-  road.style.animationDuration = animationDuration + 'ms'; 
-  console.log(road.style.animationDuration)}  
+    randomNumber = Math.floor(Math.random() * 3) * 100;
+    akadaly.style.left = randomNumber + 'px';
+    akadalytop = parseInt(window.getComputedStyle(akadaly).getPropertyValue('top'));
+  });
 
+   checkHalal = setInterval(function() {
+    pont++;
+    let jatekosBal = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'));
+    let akadalyBal = parseInt(window.getComputedStyle(akadaly).getPropertyValue('left'));
+    akadalytop = parseInt(window.getComputedStyle(akadaly).getPropertyValue('top'))
 
-     checkHalal = setInterval(function() {
-         pont++;
-         jatekosBal = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'))
-         akadalyBal = parseInt(window.getComputedStyle(akadaly).getPropertyValue('left'))
-         akadalytop = parseInt(window.getComputedStyle(akadaly).getPropertyValue('top'))
-         document.querySelector('#pont').innerHTML = 'pont: ' + pont
-         if (jatekosBal == akadalyBal && akadalytop >= 600) {
-             alert('A játéknak vége, pontjaid: ' + pont)
-             akadaly.style.animation = 'none';
-             road.style.animation = 'none';
-             akadaly.style.top = akadalytop + 'px'
-             console.log(akadalytop)
-             clearTimeout(checkHalal)
-         }
-     }, 100)
-})
-  
+    document.querySelector('#pont').innerHTML = 'pont: ' + pont;
+    if (jatekosBal == akadalyBal && akadalytop >= 400) {
+      pont -= 10;
+    }
+    
+  }, 100);
+});
