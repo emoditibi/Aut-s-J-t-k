3668d9d67f4567aad90e2cc9b54e69f2b8067bc6
TibiAHegyről <99481995+emoditibi@users.noreply.github.com>
2023-06-02 05:48:57 +0200

    2023.06.01

--- Car game/fojs.js
+++ Car game/fojs.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   let jatekos = document.querySelector('#jatekos');
   let akadaly = document.querySelector('#akadaly');
-  let pont = 0;
+  var pont = 0;
   let akadalytop = 0;
+  let penz1top = 0;
+  penz1top = parseInt(window.getComputedStyle(penz1).getPropertyValue('top'))
+ 
 
   document.addEventListener('keydown', function(e) {
     if (e.key == 'ArrowLeft') {
@@ -29,22 +32,54 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
+
   document.addEventListener('animationiteration', function() {
     randomNumber = Math.floor(Math.random() * 3) * 100;
     akadaly.style.left = randomNumber + 'px';
-    akadalytop = parseInt(window.getComputedStyle(akadaly).getPropertyValue('top'));
+
+   
   });
 
    checkHalal = setInterval(function() {
     pont++;
     let jatekosBal = parseInt(window.getComputedStyle(jatekos).getPropertyValue('left'));
+    let penz1top = parseInt(window.getComputedStyle(penz1).getPropertyValue('top'));
     let akadalyBal = parseInt(window.getComputedStyle(akadaly).getPropertyValue('left'));
     akadalytop = parseInt(window.getComputedStyle(akadaly).getPropertyValue('top'))
-
     document.querySelector('#pont').innerHTML = 'pont: ' + pont;
     if (jatekosBal == akadalyBal && akadalytop >= 400) {
       pont -= 10;
     }
+    if (jatekosBal == 0 && penz1top >= 400) {
+      pont -= 10;
+    }
+    else if (jatekosBal == 100 && penz1top >= 400) {
+      pont += 10;
+    }
+    else if (jatekosBal == 200 && penz1top >= 400) {
+      pont += 20;
+    }
+    
     
   }, 100);
 });
+var visszaszamDate = new Date().getTime() + 60000; // Az aktuális időponthoz hozzáadunk 1 percet (60000 milliszekundum)
+var visszaszamElement = document.getElementById("visszaszamlalo");
+var visszaszamInterval = setInterval(function() {
+  var most = new Date().getTime();
+  var tavolsag = visszaszamDate - most;
+  var perc = Math.floor((tavolsag % (1000 * 60 * 60)) / (1000 * 60));
+  var mp = Math.floor((tavolsag % (1000 * 60)) / 1000);
+  visszaszamElement.innerHTML = perc + " perc " + mp + " másodperc ";
+  if (tavolsag <= 0 ) {
+    clearInterval(visszaszamInterval);
+    visszaszamElement.innerHTML = "Lejárt az idő!";
+    akadaly.style.animation = 'none';
+    road.style.animation = 'none';
+    jatekos.style.animation = 'none';
+    penz1.style.animation = 'none';
+    alert('Szint készen van!')
+
+  }
+
+}, 1000);
